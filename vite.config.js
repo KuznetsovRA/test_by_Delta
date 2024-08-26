f5ab84c10c28e6bb5f302cb9d66329cfee1458d0
Kusnetsov Roman <KusnetsovRA@gmail.com>
2024-08-26 20:33:02 +0300

    1.0.6 fix: baseUrl

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/test_by_Delta/",
   build: {
     outDir: 'dist',  // Папка для финальной сборки
     assetsDir: 'assets',  // Папка для скриптов и стилей
