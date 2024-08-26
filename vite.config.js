d1360fa457ca77335bfe535f35452b5b9397c918
Kusnetsov Roman <KusnetsovRA@gmail.com>
2024-08-26 20:26:20 +0300

    1.0.5 fix: change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  build: {
+    outDir: 'dist',  // Папка для финальной сборки
+    assetsDir: 'assets',  // Папка для скриптов и стилей
+  },
 })
